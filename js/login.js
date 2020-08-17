132c9b82501fefcd50bc68582db6965c26cb4596
BVenturelli <64107807+BVenturelli@users.noreply.github.com>
2020-08-17 00:31:32 -0300

    Primer entrega 16 de Agosto

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 
+    document.getElementById("userinfo").addEventListener("submit", (evento) => {
+        evento.preventDefault();
+        window.location.href = "index.html";
+        sessionStorage.setItem("logged", true)
+        return true;
+    })
 });
